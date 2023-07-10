8e9d8ed6fd74f0c728556667703eeaee60a43b5a
shenfu1991 <xuanyuanshenfu@outlook.com>
2023-07-10 15:51:00 +0800

    Initial Commit

---
+++ Counter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by xuanyuan on 2023/7/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
